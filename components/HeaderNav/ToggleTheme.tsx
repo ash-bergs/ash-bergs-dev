a78b4585579094414ca0bae2ae444b780f53aeb0
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-07-15 01:25:36 -0400

    fix: visually hide Switch label

--- components/HeaderNav/ToggleTheme.tsx
+++ components/HeaderNav/ToggleTheme.tsx
@@ -3,18 +3,27 @@ import {
   Switch,
   FormControl,
   FormLabel,
+  VisuallyHidden,
   Tooltip,
 } from '@chakra-ui/react';
 
 const ToggleTheme = () => {
+  const { colorMode } = useColorMode();
   const { toggleColorMode } = useColorMode();
 
   return (
     <Tooltip label="Toggle theme" fontFamily="body">
       <FormControl display="flex" alignItems="center" w="min-content">
-        {/* <FormLabel htmlFor="theme-toggle" mb="0" fontWeight="400" fontSize="sm">
-        {colorMode === 'light' ? 'Dark' : 'Light'}
-      </FormLabel> */}
+        <VisuallyHidden>
+          <FormLabel
+            htmlFor="theme-toggle"
+            mb="0"
+            fontWeight="400"
+            fontSize="sm"
+          >
+            {colorMode === 'light' ? 'Dark' : 'Light'}
+          </FormLabel>
+        </VisuallyHidden>
         <Switch id="theme-toggle" onChange={toggleColorMode} />
       </FormControl>
     </Tooltip>
