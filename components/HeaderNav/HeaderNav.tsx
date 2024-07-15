f46f44b1e68be856b130777b68488dab100e54fc
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-07-15 01:11:10 -0400

    fix: make room for toggling theme on mobile and desktop in HeaderNav

--- components/HeaderNav/HeaderNav.tsx
+++ components/HeaderNav/HeaderNav.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import {
-  Box,
   Flex,
   Text,
   Menu,
@@ -9,71 +8,51 @@ import {
   MenuList,
   MenuItem,
   IconButton,
+  useColorMode,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/next-js';
 import { IoClose } from 'react-icons/io5';
 import { IoMenu } from 'react-icons/io5';
 import ToggleTheme from './ToggleTheme';
-import { useColorMode } from '@chakra-ui/react';
 
 export default function HeaderNav() {
-  const [isOpen, setIsOpen] = useState(false);
   const { colorMode } = useColorMode();
+  const [isLargerThan800] = useMediaQuery('(min-width: 800px)');
 
-  const handleToggleMenu = () => setIsOpen(!isOpen);
   return (
     <Flex
       as="nav"
       align="center"
-      justify="space-between"
-      wrap="wrap"
+      gap="4"
       padding="1.5rem"
+      justifyContent={{ base: 'space-between', md: 'initial' }}
       bg={colorMode === 'dark' ? 'gray.800' : 'white'}
       boxShadow="0px 5px 5px rgba(0, 0, 0, 0.05)"
     >
-      <Flex align="center" mr={5}>
+      <Flex minWidth="110px">
         <Link href="/" _hover={{ textDecoration: 'none' }}>
           <Text fontSize="2xl" fontWeight="bold" fontFamily="heading">
             Ash Bergs
           </Text>
         </Link>
       </Flex>
-      <Box display={{ base: 'block', md: 'none' }}>
-        <Menu isOpen={isOpen}>
-          <MenuButton
-            as="div"
-            display={{ base: 'block', md: 'none' }}
-            onClick={handleToggleMenu}
-          >
-            <IconButton
-              icon={isOpen ? <IoClose /> : <IoMenu />}
-              variant="outline"
-              aria-label={`Toggle Menu ${isOpen ? 'close' : 'open'}`}
-            />
-          </MenuButton>
-          <MenuList>
-            <MenuItem as={Link} href="/work">
-              Work
-            </MenuItem>
-            <MenuItem as={Link} href="/art">
-              Art
-            </MenuItem>
-            <MenuItem as={Link} href="/about">
-              About
-            </MenuItem>
-            <MenuItem as={Link} href="/contact">
-              Contact
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
-      <Flex
-        display={{ base: 'none', md: 'flex' }}
-        width={{ base: 'full', md: 'auto' }}
-        alignItems="center"
-        flexGrow={1}
-        gap="2"
-      >
+
+      {isLargerThan800 && <DesktopNav />}
+      {!isLargerThan800 && <MobileNav />}
+    </Flex>
+  );
+}
+
+const DesktopNav = () => {
+  return (
+    <Flex
+      display={{ base: 'none', md: 'flex' }}
+      alignItems="center"
+      w="full"
+      justifyContent="space-between"
+    >
+      <Flex gap="4" fontSize="xl" fontFamily="heading">
         <Link href="/work">Work</Link>
         <Link href="/art">Art</Link>
         <Link href="/about">About</Link>
@@ -82,4 +61,42 @@ export default function HeaderNav() {
       <ToggleTheme />
     </Flex>
   );
-}
+};
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => setIsOpen(!isOpen);
+  return (
+    <Flex gap="4" display={{ base: 'flex', md: 'none' }}>
+      <ToggleTheme />
+      <Menu isOpen={isOpen}>
+        <MenuButton
+          as="div"
+          display={{ base: 'block', md: 'none' }}
+          onClick={handleToggleMenu}
+        >
+          <IconButton
+            icon={isOpen ? <IoClose /> : <IoMenu />}
+            variant="outline"
+            aria-label={`Toggle Menu ${isOpen ? 'close' : 'open'}`}
+          />
+        </MenuButton>
+        <MenuList fontSize="xl" fontFamily="heading">
+          <MenuItem as={Link} href="/work">
+            Work
+          </MenuItem>
+          <MenuItem as={Link} href="/art">
+            Art
+          </MenuItem>
+          <MenuItem as={Link} href="/about">
+            About
+          </MenuItem>
+          <MenuItem as={Link} href="/contact">
+            Contact
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
--- components/HeaderNav/ToggleTheme.tsx
+++ components/HeaderNav/ToggleTheme.tsx
@@ -1,15 +1,23 @@
-import { useColorMode, Switch, FormControl, FormLabel } from '@chakra-ui/react';
+import {
+  useColorMode,
+  Switch,
+  FormControl,
+  FormLabel,
+  Tooltip,
+} from '@chakra-ui/react';
 
 const ToggleTheme = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { toggleColorMode } = useColorMode();
 
   return (
-    <FormControl display="flex" alignItems="center" w="min-content">
-      <FormLabel htmlFor="theme-toggle" mb="0" fontWeight="400" fontSize="sm">
+    <Tooltip label="Toggle theme" fontFamily="body">
+      <FormControl display="flex" alignItems="center" w="min-content">
+        {/* <FormLabel htmlFor="theme-toggle" mb="0" fontWeight="400" fontSize="sm">
         {colorMode === 'light' ? 'Dark' : 'Light'}
-      </FormLabel>
-      <Switch id="theme-toggle" size="md" onChange={toggleColorMode} />
-    </FormControl>
+      </FormLabel> */}
+        <Switch id="theme-toggle" onChange={toggleColorMode} />
+      </FormControl>
+    </Tooltip>
   );
 };
 
